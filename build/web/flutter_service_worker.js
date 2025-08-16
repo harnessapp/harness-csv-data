eb7e4be1bd268c09c398521b42009217dbaebd2d
joel <joelpatching@gmail.com>
2025-08-16 20:40:58 +1000

    Deploy Flutter web app

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a26ad4b629d3636fa14e937e585a75d6
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "a5cc6e76f4bb5de3532fb589f7a119e1",
+"flutter_bootstrap.js": "ee480c7bf96edb5640970bc45bce4272",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
